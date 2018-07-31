8967f944dd09767515bbcb4a25ec9037fe42b267
itman312 <lbbcsu@126.com>
2018-07-31 17:36:56 +0800

    after deploy4

--- myweb.go
+++ myweb.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request){
-	io.WriteString(w, "<h1>ni ..hhhhao ma ...80zhende ?</h1>")
+	io.WriteString(w, "<h1>ni mei de , zhongyu keyi gao yi gao le ?</h1>")
 }
 
 func main(){
